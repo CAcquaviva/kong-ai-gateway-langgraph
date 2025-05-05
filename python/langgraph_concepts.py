a8baf61a25102b77d7bc5a37d9876fb271191821
cacquaviva <claudio.acquaviva@gmail.com>
2025-05-05 10:26:28 -0300

    langgraph server

--- python/langgraph_concepts.py
+++ python/langgraph_concepts.py
@@ -2,20 +2,18 @@ from typing import TypedDict
 from langgraph.graph import StateGraph, START, END
 
 
-
-
 class State(TypedDict):
     messages: str
-    model_name: str
     response: str
 
 
-def node_1(state: dict):
+def node_1(state: State) -> State:
     completions = client.chat.completions.create(
-        model=state["model_name"],
+        model="gpt-4",
         messages=state["messages"]
     )
-    return {"response": completions.choices[0].message}
+    return {"response": completions.choices[0].message.content}
+
 
 builder = StateGraph(State)
 builder.add_node("node_1", node_1)
@@ -23,7 +21,7 @@ builder.add_edge(START, "node_1")
 builder.add_edge("node_1", END)
 
 graph = builder.compile()
-
+print(graph.get_graph().draw_ascii())
 
 
 from openai import OpenAI
@@ -33,14 +31,17 @@ client = OpenAI(
 )
 
 
-
-
 text = "What's the longitude and latitude of Seattle"
-state_dict = {}
-state_dict["messages"] = [{"role": "user", "content": text}]
-state_dict["model_name"] = "gpt-4"
+state = {"messages": [{"role": "user", "content": text}], "response": ""}
 
+print("State before invoking the graph")
+print(state)
+print("-----\n")
 
+print("Invoking the graph")
+state = graph.invoke(state)
+print("-----\n")
 
-
-print(graph.invoke({"messages": state_dict["messages"], "model_name": state_dict["model_name"]}))
+print("State after invoking the graph")
+print(state)
+print("-----\n")
